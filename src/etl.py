324cbeac6ad0a336a55a73f1db9c1a60af631de9
naimaElhmous <naimaelhmous@gmail.com>
2025-07-24 15:48:49 +0200

    Ajout de la fonction init_db() dans etl.py

---
+++ src/etl.py
@@ -0,0 +1,22 @@
+import requests
+import sqlite3
+import pandas as pd
+from datetime import datetime
+from config import API_KEY, CITY, URL, DB_PATH
+def init_db():
+    """
+    Crée la table meteo dans la base SQLite si elle n'existe pas.
+    """
+    # 1. Connexion (ouvre ou crée le fichier DB)
+    conn = sqlite3.connect(DB_PATH)
+    # 2. Exécution du DDL pour créer la table si besoin
+    conn.execute("""
+      CREATE TABLE IF NOT EXISTS meteo (
+        timestamp DATETIME,
+        temp REAL,
+        humidity INTEGER
+      )
+    """)
+    # 3. Enregistrement et fermeture
+    conn.commit()
+    conn.close()
